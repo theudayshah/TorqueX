7899b2d25c437f464f3cc42414a860627b783910
theudayshah <udayshah107.ud@gmail.com>
2025-03-11 10:58:16 +0530

    Auto-update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    //This will provide home-page
     public function index()
     {
         return view('index');
